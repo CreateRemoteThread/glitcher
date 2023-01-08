361b916e770b60a0d1453983762909dfb5e6e092
lolololol <lol@lol.com>
2023-01-09 00:19:11 +1100

    micropy avr

--- micropython/avr.py
+++ micropython/avr.py
@@ -13,10 +13,12 @@ class AVRTarget:
         return self.u1.read()
 
 class AVRProgrammer:
-    def __init__(self,pin_sck=6,pin_mosi=7,pin_miso=4,pin_cs=14):
+    def __init__(self,pin_sck=6,pin_mosi=7,pin_miso=4,pin_cs=14,pin_trig=15):
         self.p1 = SPI(0,baudrate=1000000,polarity=1,phase=1,bits=8,firstbit=SPI.MSB,sck=Pin(pin_sck),mosi=Pin(pin_mosi),miso=Pin(pin_miso))
         self.cs = Pin(pin_cs,Pin.OUT)
         self.cs.value(1)
+        self.trig = Pin(pin_trig,Pin.OUT)
+        self.trig.value(0)
         print("Initialized AVR programmer (sck=%d,mosi=%d,miso=%d,cs=%d)" % (pin_sck,pin_mosi,pin_miso,pin_cs))
 
     def enterProgramming(self):
@@ -25,9 +27,21 @@ class AVRProgrammer:
         self.p1.write_readinto(b"\xAC\x53\x00\x00",dx)
         print(":".join([hex(x) for x in dx]))
 
+    def fillMemory(self):
+        for faddr_high in range(0,0xFF):
+            for faddr_low in range(0,0xFF):
+                self.writeMemory(addr_low=faddr_low,addr_high=faddr_high,data_low=0x12,data_high=0x34)
+
+    def writeMemory(self,addr_low=0x00,addr_high=0x00,data_low=0x00,data_high=0x00):
+        self.p1.write(bytes([0x40,addr_high,addr_low,data_low]))
+        self.p1.write(bytes([0x48,addr_high,addr_low,data_high]))
+        self.p1.write(bytes([0x4C,addr_high,addr_low,0x00]))
+
     def chipErase(self):
         dx = bytearray(4)
+        self.trig.value(1)
         self.p1.write_readinto(b"\xAC\x80\x00\x00",dx)
+        self.trig.value(0)
         print(":".join([hex(x) for x in dx]))
 
     # see atmega328p datasheet, not avr isp datasheet.
@@ -36,21 +50,13 @@ class AVRProgrammer:
         self.p1.write_readinto(b"\xAC\xE0\x00" + bytes([lckbit]),dx)
         print(":".join([hex(x) for x in dx]))
 
-    def writeMemory(self,page_msb=0x00,page_lsb=0x00):
-        dx = bytearray(4)
-        mempage = bytearray(128)
-        for i in range(0,128):
-            mempage[i] = i
-        self.p1.write_readinto(bytes([0x4c,page_msb,page_lsb,0]))
-        self.p1.write(mempage)
-
     def readMemory(self,addr_b1,addr_b2):
         dx = bytearray(4)
         self.p1.write_readinto(bytes([0x20,addr_b1,addr_b2,0x00]),dx)
-        print(dx) 
+        print(":".join([hex(x) for x in dx]))
         dx = bytearray(4)
         self.p1.write_readinto(bytes([0x28,addr_b1,addr_b2,0x00]),dx)
-        print(dx) 
+        print(":".join([hex(x) for x in dx]))
 
     def exitProgramming(self):
         self.cs.value(1)
